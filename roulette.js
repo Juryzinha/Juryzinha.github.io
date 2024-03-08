c2a9b22cb115be5c19ce4fec252e1fe22c22dc49
Juryzinha <162631747+Juryzinha@users.noreply.github.com>
2024-03-08 17:36:50 -0300

    Update roulette.js

--- roulette.js
+++ roulette.js
@@ -1,89 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site de Cartas</title>
-    <link rel="stylesheet" href="style.css">
-    <audio src="bundia.mp3" id="aud1"></audio>
-</head>
-<body>
-    <div class="container">
-        <div class="card" onclick="flipCard(this)">
-            <div class="front">1</div>
-            <div class="back"><p>Que parte do corpo é mais afetada pela tuberculose?</p></div>
-        </div>
+var multiplicador = 7;
 
-        <div class="card" onclick="flipCard(this)">
-            <div class="front">2</div>
-            <div class="back"><p>Como a tuberculose é transmitida?</p></div>
-        </div>
+$('.roulett').click(function(){
+    $('.roulett').css('pointer-events', 'none');
+    $('.colors').removeClass('animation');
+    var color = colorAleatorio(1, 360);
 
-        <div class="card" onclick="flipCard(this)">
-            <div class="front">3</div>
-            <div class="back"><p>Uma pessoa com tuberculose pode trabalhar?</p></div>
-        </div>
+    $('#animation').empty().append(
+        '@keyframes animation{'+
+        '0%{transform: rotate(0deg)}'+
+        '100%{transform: rotate('+color+'deg)}'+
+        '}'
+    );
 
-        <div class="card" onclick="flipCard(this)">
-            <div class="front">4</div>
-            <div class="back"><p>Uma pessoa pode contrair a bactéria da TB e não desenvolver a doença?</p></div>
-        </div>
+    setTimeout(function(){
+        $('.colors').addClass('animation');
+    }, 1);
 
-        <div class="card" onclick="flipCard(this)">
-            <div class="front">5</div>
-            <div class="back"><p>Onde os medicamentos da tuberculose são disponibili-zados?</p></div>
-        </div>
+    setTimeout(function(){
+        if(color > 0 && color <= (90 + (360 * multiplicador))){
+            $('#roulette').css('background-color', 'var(--bg-b)');
+        } else if(color > (90 + (360 * multiplicador)) && color <= (180 + (360 * multiplicador))){
+            $('#roulette').css('background-color', 'var(--bg-y)');
+        } else if(color > (180 + (360 * multiplicador)) && color <= (270 + (360 * multiplicador))){
+            $('#roulette').css('background-color', 'var(--bg-g)');
+        } else if(color > (270 + (360 * multiplicador))){
+            $('#roulette').css('background-color', 'var(--bg-r)');
+        }
+        $('.roulett').css('pointer-events', 'initial');
+    }, 4000);
 
-        <div class="card" onclick="flipCard(this)">
-            <div class="front">6</div>
-            <div class="back"><p>A tuberculose e transmitida pelo compartilha-mento de objetos?</p></div>
-        </div>
+    setTimeout(function() {
+        $('#roulette').css('background-color', 'var(--bg-cont-b)');
+    },10000);
+});
 
-        <div class="card" onclick="flipCard(this)">
-            <div class="front">7</div>
-            <div class="back"><p>Somente pessoas com AIDS tem tuberculose?</p></div>
-        </div>
-
-        <div class="card" onclick="flipCard(this)">
-            <div class="front">8</div>
-            <div class="back"><p>É possível se prevenir da TB com vacina?</p></div>
-        </div>
-
-        <div class="card" onclick="flipCard(this)">
-            <div class="front">9</div>
-            <div class="back"><p>Qual é a cor do mês da Tuberculose?</p></div>
-        </div>
-
-        <div class="card" onclick="flipCard(this)">
-            <div class="front">10</div>
-            <div class="back"><p>Uma pessoa com HIV, pode pegar tuberculose?</p></div>
-        </div>
-    </div>
-
-    <div id="roulette">
-        
-    </div>
-
-    <div class="contenedor">
-        <div class="roulett">
-            <div class="colors">
-                <div class="colorOne"></div>
-                <div class="colorTwo"></div>
-                <div class="colorThree"></div>
-                <div class="colorFour"></div>
-            </div>
-        </div>
-    </div>
-    <style id="animation"></style>
-    <div id="premios">
-        <h4 class="red"> premio vermelho </h4>
-        <h4 class="green"> premio verde </h4>
-        <h4 class="blue"> premio azul </h4>
-        <h4 class="orange"> premio amarelo </h4>
-    </div>
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="script.js"></script>
-    <script src="roulette.js"></script>
-</body>
-</html>
+function colorAleatorio(min, max) {
+    var colorAleatorio = ((Math.random() * (max - min) + min).toFixed(0)) * 1;
+    colorAleatorio = colorAleatorio + (360 * multiplicador);
+    return colorAleatorio;
+}
